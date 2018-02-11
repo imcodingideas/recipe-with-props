39d0bdd970d702ec85da87dedceb75a930046da3
Joseph Chambers <joseph@michael-chambers.com>
2018-02-11 14:26:15 -0700

    Added list, and PropTypes

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Recipe from './Recipe'
+import RecipeList from './RecipeList'
 import Navbar from './Navbar'
 import './RecipeApp.css'
 
@@ -8,12 +8,7 @@ export default class RecipeApp extends Component {
     return (
       <div className='App'>
         <Navbar />
-        <Recipe
-          title='Spaghetti'
-          ingredients={['flour', 'water']}
-          instructions='Mix ingredients'
-          img='spaghetti.jpg'
-          />
+        <RecipeList />
       </div>
     )
   }
