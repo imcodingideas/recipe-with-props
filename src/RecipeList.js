ee5103bc3e765e8af65bbf3be8b37a42bd4fe7b8
Joseph Chambers <joseph@michael-chambers.com>
2018-02-11 18:32:35 -0700

    Deleting Recipes, Adding new items to state, passing functions and props.

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -5,16 +5,18 @@ import './RecipeList.css'
 
 export default class RecipeList extends Component {
   static propTypes = {
-    recipes: PropTypes.arrayOf(PropTypes.object).isRequired
+    recipes: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onDelete: PropTypes.func.isRequired
   }
 
-  render() {
-    const recipes = this.props.recipes.map((r, id) => (
-      <Recipe key={r.id} {...r} />
+  render () {
+    const {onDelete} = this.props
+    const recipes = this.props.recipes.map((r, index) => (
+      <Recipe key={r.id} {...r} onDelete={onDelete} />
     ))
 
     return (
-      <div className="recipe-list">
+      <div className='recipe-list'>
         {recipes}
       </div>
     )
